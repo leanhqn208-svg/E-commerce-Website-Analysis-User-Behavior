df213735d1cf39e388fe6d2d9a432ec4c546d172
leanhqn208-svg <leanhqn208@gmail.com>
2026-04-02 15:53:01 +0700

    Create query_01.sql

---
+++ sql/query_01.sql
@@ -0,0 +1,12 @@
+query_01.sql
+
+--Query 01: calculate total visit, pageview, transaction for Jan, Feb and March 2017 (order by month)	
+SELECT
+  format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
+  SUM(totals.visits) AS visits,
+  SUM(totals.pageviews) AS pageviews,
+  SUM(totals.transactions) AS transactions,
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`
+WHERE _TABLE_SUFFIX BETWEEN '0101' AND '0331'
+GROUP BY 1
+ORDER BY 1;
